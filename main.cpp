bde2b26ac52d2145f821ebe0773b340efe1576fa
daniel <dale@create.aau.dk>
2022-09-26 12:46:09 +0200

    Updates to CLI

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
 //https://support.thinklucid.com/app-note-helios-3d-point-cloud-with-rgb-color/
-//https://gist.github.com/zhou-chao/7a7de79de47c652196f1
 
 #include "ArenaApi.h"
 #include "SaveApi.h"
 
 #include "signal.h"
 #include "unistd.h"
-#include <fstream>
+#include "fstream"
 
 #include "opencv2/opencv.hpp"
 #include "opencv2/highgui.hpp"
@@ -18,13 +17,11 @@
 #define NUM_IMAGES 25
 #define PLY_FILE_NAME "pc.ply"
 #define IMG_FILE_NAME "heatmap.jpg"
-#define CSV_FILE_NAME "depth.csv"
-#define OPERATING_MODE "Distance1500mm" //options are Distance1500mm, Distance6000mm
-//Distance1500mm@30FPS, Distance6000mm@15FPS
-
 int FLAG_KILL_STREAM = 0;
+std::string operating_mode;
 
 
+//This function is used to safely stop a camera stream
 void signal_callback_handler(int signum)
 {
    std::cout << "Caught signal " << signum << std::endl;
@@ -33,6 +30,7 @@ void signal_callback_handler(int signum)
 }
 
 
+//Get a connected Helios camera or exit the program if no camera stis connected
 Arena::IDevice* get_device(Arena::ISystem* pSystem)
 {
 	pSystem->UpdateDevices(100);
@@ -48,11 +46,12 @@ Arena::IDevice* get_device(Arena::ISystem* pSystem)
 }
 
 
+//Visualise intensity image as a gray image
 void show_gray_image(Arena::IDevice* pDevice)
 {
 	//set streaming parameters - pixel format, operating mode, and data transfer options
 	Arena::SetNodeValue<GenICam::gcstring>(pDevice->GetNodeMap(), "PixelFormat", "Mono8");
-	Arena::SetNodeValue<GenICam::gcstring>(pDevice->GetNodeMap(), "Scan3dOperatingMode", OPERATING_MODE);
+	Arena::SetNodeValue<GenICam::gcstring>(pDevice->GetNodeMap(), "Scan3dOperatingMode", operating_mode.c_str());
 	Arena::SetNodeValue<bool>(pDevice->GetTLStreamNodeMap(), "StreamAutoNegotiatePacketSize", true);
 	Arena::SetNodeValue<bool>(pDevice->GetTLStreamNodeMap(), "StreamPacketResendEnable", true);
 
@@ -79,6 +78,7 @@ void show_gray_image(Arena::IDevice* pDevice)
 }
 
 
+//calculate heatmap coloring based on the depth data of the input image
 Arena::IImage* apply_heatmap(Arena::IImage* pImage, float scale)
 {
 	// prepare info from input buffer
@@ -111,7 +111,7 @@ Arena::IImage* apply_heatmap(Arena::IImage* pImage, float scale)
 	const double RGBmax = 255;
 	double redColorBorder, yellowColorBorder, greenColorBorder, cyanColorBorder, blueColorBorder;
 
-	if (OPERATING_MODE == "Distance1500mm")
+	if (operating_mode == "Distance1500mm")
 	{
 		redColorBorder = 0;
 		yellowColorBorder = 375;
@@ -221,11 +221,12 @@ Arena::IImage* apply_heatmap(Arena::IImage* pImage, float scale)
 }
 
 
+//Get the intensity image and calculate heatmap for it
 void show_heatmap_image(Arena::IDevice* pDevice)
 {
 	//set operating and streaming nodes
 	Arena::SetNodeValue<GenICam::gcstring>(pDevice->GetNodeMap(), "PixelFormat", "Coord3D_ABCY16");
-	Arena::SetNodeValue<GenICam::gcstring>(pDevice->GetNodeMap(), "Scan3dOperatingMode", "Distance6000mm");
+	Arena::SetNodeValue<GenICam::gcstring>(pDevice->GetNodeMap(), "Scan3dOperatingMode", "Distance1500mm");
 	Arena::SetNodeValue<bool>(pDevice->GetTLStreamNodeMap(), "StreamAutoNegotiatePacketSize", true);
 	Arena::SetNodeValue<bool>(pDevice->GetTLStreamNodeMap(), "StreamPacketResendEnable", true);
 	Arena::SetNodeValue<GenICam::gcstring>(pDevice->GetNodeMap(), "Scan3dCoordinateSelector", "CoordinateC");
@@ -233,7 +234,7 @@ void show_heatmap_image(Arena::IDevice* pDevice)
 	//sets all relevant nodes targeted towards getting smooth results
 	Arena::SetNodeValue<GenICam::gcstring>(pDevice->GetNodeMap(), "ExposureTimeSelector", "Exp1000Us");
 	Arena::SetNodeValue<GenICam::gcstring>(pDevice->GetNodeMap(), "ConversionGain", "Low");
-	//Arena::SetNodeValue<int64_t>(pDevice->GetNodeMap(), "Scan3dImageAccumulation", 4);
+	Arena::SetNodeValue<int64_t>(pDevice->GetNodeMap(), "Scan3dImageAccumulation", 4);
 	Arena::SetNodeValue<bool>(pDevice->GetNodeMap(), "Scan3dSpatialFilterEnable", true);
 	Arena::SetNodeValue<bool>(pDevice->GetNodeMap(), "Scan3dConfidenceThresholdEnable", true);
 
@@ -258,11 +259,12 @@ void show_heatmap_image(Arena::IDevice* pDevice)
 }
 
 
+//Save depth data as a ply file
 void save_image_as_ply(Arena::IDevice* pDevice)
 {	
 	//set operating and streaming nodes
 	Arena::SetNodeValue<GenICam::gcstring>(pDevice->GetNodeMap(), "PixelFormat", "Coord3D_ABC16");
-	Arena::SetNodeValue<GenICam::gcstring>(pDevice->GetNodeMap(), "Scan3dOperatingMode", OPERATING_MODE);
+	Arena::SetNodeValue<GenICam::gcstring>(pDevice->GetNodeMap(), "Scan3dOperatingMode", operating_mode.c_str());
 	Arena::SetNodeValue<bool>(pDevice->GetTLStreamNodeMap(), "StreamAutoNegotiatePacketSize", true);
 	Arena::SetNodeValue<bool>(pDevice->GetTLStreamNodeMap(), "StreamPacketResendEnable", true);
 
@@ -317,8 +319,10 @@ void save_image_as_ply(Arena::IDevice* pDevice)
 }
 
 
+//Stream intensity data as grayscale image
 void stream_data(Arena::IDevice* pDevice)
 {
+	//Setup nodes for streaming
 	Arena::SetNodeValue<GenICam::gcstring>(pDevice->GetNodeMap(), "PixelFormat", "Mono8");
 	Arena::SetNodeValue<GenICam::gcstring>(pDevice->GetNodeMap(), "AcquisitionMode", "Continuous");
 	Arena::SetNodeValue<GenICam::gcstring>(pDevice->GetNodeMap(), "Scan3dOperatingMode", "Distance6000mm");
@@ -326,6 +330,7 @@ void stream_data(Arena::IDevice* pDevice)
 	Arena::SetNodeValue<bool>(pDevice->GetTLStreamNodeMap(), "StreamAutoNegotiatePacketSize", true);
 	Arena::SetNodeValue<bool>(pDevice->GetTLStreamNodeMap(), "StreamPacketResendEnable", true);
 	
+	//Smooth results
 	Arena::SetNodeValue<GenICam::gcstring>(pDevice->GetNodeMap(), "ExposureTimeSelector", "Exp250Us");
 	Arena::SetNodeValue<GenICam::gcstring>(pDevice->GetNodeMap(), "ConversionGain", "High");
 	//Arena::SetNodeValue<int64_t>(pDevice->GetNodeMap(), "Scan3dImageAccumulation", 4);
@@ -338,11 +343,11 @@ void stream_data(Arena::IDevice* pDevice)
 	while(1)
 	{
 		Arena::IImage* pImage = pDevice->GetImage(IMAGE_TIMEOUT);
-		size_t size = pImage->GetSizeFilled();
-		size_t width = pImage->GetWidth();
-		size_t height = pImage->GetHeight();
-		GenICam::gcstring pixelFormat = GetPixelFormatName(static_cast<PfncFormat>(pImage->GetPixelFormat()));
-		uint64_t timestampNs = pImage->GetTimestampNs();
+		//size_t size = pImage->GetSizeFilled();
+		//size_t width = pImage->GetWidth();
+		//size_t height = pImage->GetHeight();
+		//GenICam::gcstring pixelFormat = GetPixelFormatName(static_cast<PfncFormat>(pImage->GetPixelFormat()));
+		//uint64_t timestampNs = pImage->GetTimestampNs();
 
 		//std::cout << " (" << size << " bytes; " << width << "x" << height << "; " << pixelFormat << "; timestamp (ns): " << timestampNs << ")";
 		cv::Mat img = cv::Mat((int)pImage->GetHeight(), (int)pImage->GetWidth(), CV_8UC1, (void *)pImage->GetData());
@@ -360,13 +365,14 @@ void stream_data(Arena::IDevice* pDevice)
 	// Stop stream
 	//    Stop the stream after all images have been requeued. Failing to stop
 	//    the stream will leak memory.
+	cv::destroyAllWindows();
 	pDevice->StopStream();
 }
 
 
 void get_depth_map(Arena::IDevice* pDevice)
 {
-	// Get Helios XYZ data bytes and intensity data:
+	
 	Arena::SetNodeValue<GenICam::gcstring>(pDevice->GetNodeMap(), "PixelFormat", "Coord3D_ABCY16");
 	Arena::SetNodeValue<GenICam::gcstring>(pDevice->GetNodeMap(), "Scan3dOperatingMode", "Distance1500mm");
 	Arena::SetNodeValue<bool>(pDevice->GetTLStreamNodeMap(), "StreamAutoNegotiatePacketSize", true);
@@ -389,19 +395,25 @@ void get_depth_map(Arena::IDevice* pDevice)
 	Arena::SetNodeValue<GenICam::gcstring>(pDevice->GetNodeMap(), "Scan3dCoordinateSelector", "CoordinateC");
 	double z_offset_mm = Arena::GetNodeValue<double>(pDevice->GetNodeMap(), "Scan3dCoordinateOffset");
 
+	// Start stream, and get image
 	pDevice->StartStream();
 	Arena::IImage* image = pDevice->GetImage(2000);
 
+	// Create two empty OpenCV images
+	// one to encode depth coordinates (CV_32FC3) - three channels (one channel for each coordinate XYZ) of float data type (depth must be float) 
+	// one for intensity (CV_16UC1) - single channel, 16bit unsigned
 	size_t height, width;
 	height = image->GetHeight();
 	width = image->GetWidth();
-
 	cv::Mat xyz_mm = cv::Mat((int)height, (int)width, CV_32FC3);
 	cv::Mat intensity_image = cv::Mat((int)height, (int)width, CV_16UC1);
 
+	// Get Helios XYZ data bytes and intensity data:
 	const uint16_t* input_data;
 	input_data = (uint16_t*)image->GetData();
+	
 
+	// Copy the image data into the empty OpenCV images
 	for (unsigned int ir = 0; ir < height; ++ir)
 	{
 		for (unsigned int ic = 0; ic < width; ++ic)
@@ -416,18 +428,27 @@ void get_depth_map(Arena::IDevice* pDevice)
 			xyz_mm.at<cv::Vec3f>(ir, ic)[1] = (float)(y_u16 * xyz_scale_mm + y_offset_mm);
 			xyz_mm.at<cv::Vec3f>(ir, ic)[2] = (float)(z_u16 * xyz_scale_mm + z_offset_mm);
 			//std::cout<<"Pixel ("<<ir<<","<<ic<<") Distance "<<xyz_mm.at<cv::Vec3f>(ir, ic)[2]<<std::endl;
-			intensity_image.at<ushort>(ir, ic) = input_data[3]; // // Intensity value
+			// Get intensity values, these dont need to be converted (ushort == uint16_t)
+			intensity_image.at<ushort>(ir, ic) = input_data[3]; 
 			input_data += 4;
 		}
 	}
-	std::cout<<"X Distance at the center "<<xyz_mm.at<cv::Vec3f>(240, 320)[0]<<std::endl;
-	std::cout<<"Y Distance at the center "<<xyz_mm.at<cv::Vec3f>(240, 320)[1]<<std::endl;
-	std::cout<<"Z Distance at the center "<<xyz_mm.at<cv::Vec3f>(240, 320)[2]<<std::endl;
 	pDevice->StopStream();
+	std::cout<<"Distance to the center point "<<xyz_mm.at<cv::Vec3f>(240, 320)[2]<<std::endl;
+	// Visualise the intensity image and draw a circle around the center point
+	cv::Point center(320, 240);//Declaring the center point
+	int radius = 5; //Declaring the radius
+	cv::Scalar line_Color(0, 0, 0);//Color of the circle
+	int thickness = 5;//thickens of the line
+	cv::circle(intensity_image, center,radius, line_Color, thickness);//Using circle()function to draw the line//
+	cv::imshow("HLS Intensity", intensity_image);
+	cv::waitKey(0);
+	cv::destroyAllWindows();
 
+	// Optional: save point coordinates into csv files
+	/* 
 	cv::Mat channels[3];
 	cv::split(xyz_mm, channels);
-
 	std::ofstream myfile;
 	std::vector<std::string> filenames = {"x.csv", "y.csv", "z.csv"};
 	for (size_t i = 0; i < 3; i++)
@@ -436,17 +457,13 @@ void get_depth_map(Arena::IDevice* pDevice)
 		myfile<< cv::format(channels[i], cv::Formatter::FMT_CSV) << std::endl;
 		myfile.close();
 	}
-	
-	// Optional: Show the Helios intensity image
-	cv::Point center(320, 240);//Declaring the center point
-	int radius = 5; //Declaring the radius
-	cv::Scalar line_Color(0, 0, 0);//Color of the circle
-	int thickness = 5;//thickens of the line
-	cv::circle(intensity_image, center,radius, line_Color, thickness);//Using circle()function to draw the line//
-	cv::imshow("HLS Intensity", intensity_image);
-	cv::waitKey(0);
+	*/
 }
 
+
+void print(int i){
+	std::cout<<"Debug "<<i<<std::endl;
+}
 
 int main(int argc, char* argv[])
 {
@@ -454,34 +471,51 @@ int main(int argc, char* argv[])
 	bool exceptionThrown = false;
 	signal(SIGINT, signal_callback_handler);
 
-	if(argc>2){
-		std::cout<< "Please insert only one input argument. For help, run ./main -help \n";
+	if(argc>3){
+		std::cout<< "Please insert only two input arguments. For help, run ./main -help \n";
 		exit(1);
 	} 
+	print(1);
 
-	if(argc<2){
-		std::cout<< "Please insert an input argument. For help, run ./main -help \n";
+	if(argc<3 && std::string(argv[1]) != "-help"){
+		std::cout<< "Please insert two inputs argument. For help, run ./main -help \n";
 		exit(1); 
 	}
+	print(2);
 
 	if (std::string(argv[1]) != "-help" && std::string(argv[1]) != "-gray" &&
 	std::string(argv[1]) != "-heatmap" && std::string(argv[1]) != "-ply" &&
-	std::string(argv[1]) != "-stream" && std::string(argv[1]) != "-depth" ) {
+	std::string(argv[1]) != "-stream" && std::string(argv[1]) != "-depth") 
+	{
 		std::cout<< "Unrecognized input argument. For help, run ./main -help \n";
 		exit(1);
 	}
+	print(3);
 
-	
 	if (std::string(argv[1]) == "-help"){
-		std::cout<<"Input arguments are:\n"
+		std::cout<<"First select functionality. Input arguments are:\n"
 		<<TAB1<<"-gray"<<TAB1<<"Capture an intensity image, and visualise it as a grayscale image using OpenCV.\n"
 		<<TAB1<<"-heatmap"<<TAB1<<"Capture depth data image, apply heatmat based on the measured depth, and visualise it as an rgb image using OpenCV.\n"
 		<<TAB1<<"-ply"<<TAB1<<"Capture depth data, save it as a ply file.\n"
 		<<TAB1<<"-stream"<<TAB1<<"Stream intensity data.\n"
-		<<TAB1<<"-depth"<<TAB1<<"Capture depth data and save it into a .csv file.\n";
+		<<TAB1<<"Then select the operating mode. Input arguments are :\n"
+		<<TAB1<<"-6000"<<TAB1<<"Operating mode is set to Distance6000mm (Far Mode). This mode operates at 15 FPS.\n"
+		<<TAB1<<"-1500"<<TAB1<<"Operating mode is set to Distance1500mm (Near Mode). This mode operates at 30 FPS.\n"
+		<<TAB1<<"For example, ./main -stream -6000 will launch stream in a Far Mode.\n";
 		exit(1);
 	}
+	print(4);
+	
+	if (std::string(argv[2]) != "-6000" && std::string(argv[2]) != "-1500")
+	{
+		std::cout<< "Unrecognized input argument. For help, run ./main -help \n";
+		exit(1);
+	}
+	print(5);
 
+	if (std::string(argv[2]) == "-6000") operating_mode = "Distance6000mm";
+	if (std::string(argv[2]) == "-1500") operating_mode = "Distance1500mm";
+	print(6);
 	
 	try
 	{
